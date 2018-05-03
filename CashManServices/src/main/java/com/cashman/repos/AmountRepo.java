261272ca0f58c599fa5ca67634e4b790e4a59400
Abhisekh Joshi <ajoshi@Mssdomain.local>
2018-05-03 12:23:34 +0530

    added new B

--- CashManServices/src/main/java/com/cashman/repos/AmountRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cashman.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cashman.model.Amount;
-
-public interface AmountRepo extends JpaRepository<Amount, Long> {
-
-}
